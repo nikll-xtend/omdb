810c88298d1860ee8b808e265d5a77e16edeafdf
nikll-xtend <nikll.xtend@gmail.com>
2025-03-14 14:44:44 +0530

    code change for heroku

--- config/Database.php
+++ config/Database.php
@@ -1,28 +1,31 @@
 <?php
-class Database {
+class Database
+{
     private static $instance = null;
     private $conn;
 
-    private function __construct() {
-        $env = parse_ini_file(__DIR__ . '/../.env'); 
+    private function __construct()
+    {
+        $env = parse_ini_file(__DIR__ . '/../.env');
 
-        $host = $env['DB_HOST'];
-        $dbname = $env['DB_NAME'];
-        $user = $env['DB_USER'];
-        $pass = $env['DB_PASS'];
+        $host = getenv('DB_HOST') ?: (file_exists($env) ? parse_ini_file($env)['DB_HOST'] : '');
+        $user = getenv('DB_USER') ?: (file_exists($env) ? parse_ini_file($env)['DB_USER'] : '');
+        $pass = getenv('DB_PASS') ?: (file_exists($env) ? parse_ini_file($env)['DB_PASS'] : '');
+        $dbname = getenv('DB_NAME') ?: (file_exists($env) ? parse_ini_file($env)['DB_NAME'] : '');
+
 
         $this->conn = new mysqli($host, $user, $pass, $dbname);
-        
+
         if ($this->conn->connect_error) {
             die("Database Connection Failed: " . $this->conn->connect_error);
         }
     }
 
-    public static function getInstance() {
+    public static function getInstance()
+    {
         if (!self::$instance) {
             self::$instance = new Database();
         }
         return self::$instance->conn;
     }
 }
-?>
